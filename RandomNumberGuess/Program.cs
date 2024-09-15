a4cdc25b112e492bfa26c52bd3d3a13dc814c66c
lebid-ol <swan.programmer@gmail.com>
2024-09-15 14:58:01 +0300

    first commit

---
+++ RandomNumberGuess/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.Design;
+using System.Text;
+
+class RandomNumberGenerator
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.Unicode;
+        
+        Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
+        Console.WriteLine("Я загадал число между: 1 и 100.");
+        Console.WriteLine("Попробуйте угадать.");
+
+        // Создаем объект для генерации чисел
+        Random rand = new Random();
+            
+        // Создаем переменную для хранения случайного числа
+        int random_next = rand.Next(1, 101);
+        
+        // Создаем переменную для хранения числа попыток
+        int attempt = 0;
+        
+        // 1. Создаем цикл, который будет выполняться до тех пор, пока не будет угадано число
+        while (true) {
+            if (attempt = 0; attempt < 10; attempt++) {
+
+
+                // 2.2. Считываем число с клавиатуры и сохраняем в переменную
+                bool resultinput = int.TryParse(Console.ReadLine(), out int result);
+                while (!resultinput || result <= 0 || result >= 101)
+                {
+                    Console.Write("Данные некорретны. Введите числовое значение от 1 до 100: ");
+                    resultinput = int.TryParse(Console.ReadLine(), out result);
+                }
+
+                // 2.3. Сравниваем загаданное число с введенным пользователем
+                if (result == random_next)
+                {
+                    // 2.4. Если числа равны, то выводим сообщение о победе и о количестве попыток
+                    Console.WriteLine("Вы выиграли, угаданное число " + result + " , количество попыток " + attempt);
+                    break;
+                }
+
+                // 2.5. Если число пользователя больше загаданного, то выводим сообщение, что число больше загаданного
+                else if (result > attempt)
+                {
+                    Console.WriteLine("Число больше загаданного");
+                }
+                else
+                {
+                    Console.WriteLine("Число меньше загаданного");
+                }
+            
+            else
+            {
+
+            }
+               
+
+            }
+}
