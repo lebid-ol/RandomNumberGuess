12529188e135fda467f0d839df9ef11265e1c1a5
lebid-ol <swan.programmer@gmail.com>
2024-09-28 20:50:40 +0300

    improved version

--- RandomNumberGuess/Program.cs
+++ RandomNumberGuess/Program.cs
@@ -7,56 +7,57 @@ class RandomNumberGenerator
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
-        
+
         Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
         Console.WriteLine("Я загадал число между: 1 и 100.");
-        Console.WriteLine("Попробуйте угадать.");
+        Console.WriteLine("Попробуйте угадать. У вас 10 попыток");
 
         // Создаем объект для генерации чисел
         Random rand = new Random();
-            
+
         // Создаем переменную для хранения случайного числа
         int random_next = rand.Next(1, 101);
-        
+
         // Создаем переменную для хранения числа попыток
         int attempt = 0;
-        
+
         // 1. Создаем цикл, который будет выполняться до тех пор, пока не будет угадано число
-        while (true) {
-            if (attempt = 0; attempt < 10; attempt++) {
+        while (attempt < 10)
+        {
+            attempt++;
+            Console.WriteLine("Попытка " + attempt);
 
+            // 2.2. Считываем число с клавиатуры и сохраняем в переменную
+            bool resultinput = int.TryParse(Console.ReadLine(), out int result);
+            while (!resultinput || result <= 0 || result >= 101)
+            {
+                Console.Write("Данные некорретны. Введите целое число от 1 до 100: ");
+                resultinput = int.TryParse(Console.ReadLine(), out result);
+            }
 
-                // 2.2. Считываем число с клавиатуры и сохраняем в переменную
-                bool resultinput = int.TryParse(Console.ReadLine(), out int result);
-                while (!resultinput || result <= 0 || result >= 101)
-                {
-                    Console.Write("Данные некорретны. Введите числовое значение от 1 до 100: ");
-                    resultinput = int.TryParse(Console.ReadLine(), out result);
-                }
+            // 2.3. Сравниваем загаданное число с введенным пользователем
+            if (result == random_next)
+            {
+                // 2.4. Если числа равны, то выводим сообщение о победе и о количестве попыток
+                Console.WriteLine("Вы выиграли, угаданное число " + result + " , количество попыток " + attempt);
+                break;
+            }
 
-                // 2.3. Сравниваем загаданное число с введенным пользователем
-                if (result == random_next)
-                {
-                    // 2.4. Если числа равны, то выводим сообщение о победе и о количестве попыток
-                    Console.WriteLine("Вы выиграли, угаданное число " + result + " , количество попыток " + attempt);
-                    break;
-                }
-
-                // 2.5. Если число пользователя больше загаданного, то выводим сообщение, что число больше загаданного
-                else if (result > attempt)
-                {
-                    Console.WriteLine("Число больше загаданного");
-                }
-                else
-                {
-                    Console.WriteLine("Число меньше загаданного");
-                }
-            
+            // 2.5. Если число пользователя больше загаданного, то выводим сообщение, что число больше загаданного
+            if (result > random_next)
+            {
+                Console.WriteLine("Число больше загаданного");
+            }
             else
             {
-
+                Console.WriteLine("Число меньше загаданного");
             }
+        }
+                
+            Console.WriteLine("Число попыток равно 10. Вы проиграли. Загаданное число было " + random_next);
+        }
+    }
                
 
-            }
-}
+            
+
